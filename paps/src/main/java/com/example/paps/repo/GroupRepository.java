20c0e56563b1eb77b7f6d867cbe7dd127a341b56
Burnysheff <77582124+Burnysheff@users.noreply.github.com>
2024-03-11 13:40:30 +0300

    Delete paps directory

--- paps/src/main/java/com/example/paps/repo/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.paps.repo;
-
-import com.example.paps.model.Group;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends CrudRepository<Group, Long> {
-}
--- paps/src/main/java/com/example/paps/repo/RuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.paps.repo;
-
-import com.example.paps.model.Rule;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RuleRepository extends CrudRepository<Rule, Long> {
-}
